dc1622a9e9f4090bb669523865209cbaadc5e3ee
shouriadde23 <shouriadde1@gmail.com>
2021-01-23 16:38:25 -0800

    jobs in city and top 5 roles added, still need to test

--- app.py
+++ app.py
@@ -20,11 +20,70 @@ def search():
 @app.route('/results/<query>')
 def results(query=None):
     if query is not None:
+
+
+#         jobsInCity = []
+# city = "Noida"
+# for i in range (len(df)):
+#     if city in df.iloc[i]['Location']:
+#         jobsInCity.append(df.iloc[i]) 
+
+
+
+#         rolesInCity = {}
+# for i in range (len(jobsInCity)):
+#     if (jobsInCity[i]['Role'] in rolesInCity):
+#         rolesInCity[jobsInCity[i]['Role']] += 1
+#     else:
+#         rolesInCity[jobsInCity[i]['Role']] = 1
+
+
+# role = "Software Developer"
+# jobs = []
+# for i in range (len(jobsInCity)):
+#     if role in jobsInCity[i]['Role']:
+#         jobs.append(jobsInCity[i])
+
+
+# jobs is all the jobs of the chosen role in the chosen city
+
+#rolesInCity is a list of all the roles in the chosen city
+
+
+#jobsInCity is a list of all the jobs in the city
+        jobsInCity = []
         # breh
         # need to match cities info with our query value
-        
+        for(i in range(len(data))):
+            if query in data.iloc[i]['Location']:
+                jobsInCity.append(df.iloc[i])
         # we need to find the most frequent jobs with appropriate city match
+        rolesInCity = []
+        for i in range (len(jobsInCity)):
+            if (jobsInCity[i]['Role'] in rolesInCity):
+                rolesInCity[jobsInCity[i]['Role']] += 1
+            else:
+                rolesInCity[jobsInCity[i]['Role']] = 1
 
+        # find the top 5 common roles 
+        rolesInCity.sort(reverse = True)
+        top5Roles = rolesInCity[:5]
+
+        # find the number of jobs available to each role in the city
+        # jobs = []
+        # count = 0
+        # role = top5Roles[count]
+
+        # for i in range (len(top5Roles)):
+        #     if role in jobsInCity[i]['Role']:
+        #         count += 1
+        #         jobs.append(jobsInCity[i])
+        
+        # find the average salary for each role in the city
+        
+        # salaries = []
+        # for i in range(len(jobsInCity)):
+            
 
         results = ["Textiles Manager", "Supply Chain Analyst", "Telemarketer"]
         return render_template('results.html', query=query.capitalize(), results=results)
@@ -35,12 +94,7 @@ def results(query=None):
 def explore(query=None, focus=None):
     if query is not None and focus is not None:
         # breh
-        query_jobs = ["Job 1", "Job 2", "Job 3"]
-        materials = ["YT 123","Khan","blah"]
-        return render_template('explore.html', 
-                               query=query.capitalize(), 
-                               focus=focus, 
-                               jobs=query_jobs, 
-                               materials=materials)
+        results = ["Textiles Manager", "Supply Chain Analyst", "Telemarketer"]
+        return render_template('explore.html', query=query.capitalize(), focus=focus)
     else:
         return redirect((url_for('results', query=query)))
